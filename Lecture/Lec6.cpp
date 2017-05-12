3dd46e3d62c03d027a321e2e40509b7ff48f81c0
cindyywang <ying2ra@gmail.com>
2017-05-11 18:55:00 -0700

    a5

---
+++ Lecture/Lec6.cpp
@@ -0,0 +1,85 @@
+//Lec_6
+#include <iostream>
+
+int()
+{
+//Review
+/*
+Liberal
+2,3,999
+145,0.00091,3.45e-9
+'a','z','\t'
+true,false
+"Hello, string"
+*/
+/*
+Variable
+myNum, isCorrect(identifier),
+int
+double
+bool
+string
+*/
+/*
+Expression
+numerical expression
++,-,*,/,%
+boolean expression
+relation op
+>=, <=, ++, !=
+Logical op
+Or, And, Not
+*/
+/*
+Assignment
+<lhs> = <rhs>
+var      exp
+*/
+/*
+Conditional
+
+*/
+
+//New class
+/*
+Strings
+#include <string>
+str9ng str;
+str = "Hello';
+concatenation +
+cout<<str+" World";
+str = "Hello" + " World";//One of them has to be a real literal
+//Like string("Hello')
+cin >> str;
+//Only read the first token
+getline()
+getline(cin, str);
+cin>>str1>>str2;
+cin >> num1 >> num2>>....;
+
+*/
+
+/*
+Things you can't do
+cin >> "Hello";
+cin << x;
+cout >> y;
+*/
+
+/*
+#include<sstream>
+str->num
+double d;
+string str = "13.4";
+istringstream(str)>>d;
+
+num->str
+ostringstream cnvrt;
+cnvrt<<d;
+str = cnvrt.str();
+*/
+
+
+
+}
+
---
+++ Lecture/Lec8.cpp
@@ -0,0 +1,9 @@
+/*
+return 0;
+return 1?
+continue to the rest?
+exit(int)
+//exit the entrie program and capture the error
+exit(0)?
+*/
+
